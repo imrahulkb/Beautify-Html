e55ab7decfb88ab95f84238a316665d04cb0e265
imrahulkb <therahulrrj007@gmail.com>
2023-04-02 15:54:14 +0530

    Navbar fixed and buttons rearranged

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const beautifiedCode = document.getElementById('beautified-code');
 function clearInput() {
 	document.getElementById("html-code").value = "";
 }
+
 async function pasteFromClipboard() {
 	const text = await navigator.clipboard.readText();
 	document.getElementById("html-code").value = text;
 }
+
 beautifyButton.addEventListener('click', () => {
 	// Import the HTML as a string
 	const html = document.getElementById('html-code').value;
@@ -27,18 +29,14 @@ beautifyButton.addEventListener('click', () => {
 	}).replace(/[\n]+/g, '\n').replace(/^\n|\n$/g, '');
 	document.getElementById('beautified-code').textContent = beautifiedHtml;
 })
+
 const copyButton = document.getElementById('copy-button');
+
 copyButton.addEventListener('click', () => {
-	// Create a temporary textarea element
 	const tempTextarea = document.createElement('textarea');
-	// Set the value of the textarea to the beautified code
 	tempTextarea.value = beautifiedCode.textContent;
-	// Add the textarea to the page
 	document.body.appendChild(tempTextarea);
-	// Select the text in the textarea
 	tempTextarea.select();
-	// Copy the text to the clipboard
 	document.execCommand('copy');
-	// Remove the textarea from the page
 	document.body.removeChild(tempTextarea);
 });
